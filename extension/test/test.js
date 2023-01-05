cfea868659a35fb17e04d9a6fdb5519adb2cb719
ern0 <ern0@linkbroker.hu>
2023-01-05 16:51:48 +0100

    added test runner

--- extension/test/test.js
+++ extension/test/test.js
@@ -1,4 +1,28 @@
-const ext = require("../script/switch-star-tags.js");
+ext = require("../script/switch-star-tags.js");
 
-var name = ext.parse_game_id("dfdf");
-console.log(name);
+function run_tests() {
+
+	for (var index in globalThis) {
+			var fn = globalThis[index];
+			t = ({}).toString.call(fn).toString();
+			if (t != "[object Function]") continue;
+			if (fn.name.substring(0, 5) != "test_") continue;
+
+			fn.call();
+
+	}
+}
+
+function assert() {
+	console.log("ass");
+}
+
+
+test_nope = () =>
+{
+    assert();
+    var name = ext.parse_game_id("dfdf");
+    console.log(name);
+}
+
+run_tests();
