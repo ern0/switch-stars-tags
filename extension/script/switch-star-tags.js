f0a0316e7522fd50c74ba02c089778503077f84a
ern0 <ern0@linkbroker.hu>
2023-01-07 19:01:18 +0100

    Added site detect with tests

--- extension/script/switch-star-tags.js
+++ extension/script/switch-star-tags.js
@@ -2,13 +2,30 @@ function init_extension() {
 	document.body.style.border = "12px solid blue";
 }
 
-function parse_game_id(url) {
+function detect_site(url) {
 
-	return "lofasz";
+	if (url.includes("dekudeals.com/")) return "dd";
+	if (url.includes("nintendo.co.uk/")) return "uk";
+	if (url.includes("nintendo.com/")) return "us";
+
+	return "n.a.";
+}
+
+function parse_game_name(site, url) {
+
+	if (url.slice(-1) == "/") url = url.slice(0, -1);
+	var lastSlashPos = url.lastIndexOf("/") + 1;
+	var name = "n.a.";
+
+	if (site == "dd") name = url.slice(lastSlashPos);
+	if (site == "uk") name = url.slice(lastSlashPos, url.lastIndexOf("-"));
+	if (site == "us") name = url.slice(lastSlashPos);
+
+	return name.toLowerCase().replaceAll("-", "_");
 }
 
 if (typeof(document) == "object") {
 	init_extension();
 } else {
-	module.exports = { parse_game_id };
+	module.exports = { parse_game_name, detect_site };
 }
--- extension/test/test.js
+++ extension/test/test.js
@@ -1,28 +1,56 @@
 ext = require("../script/switch-star-tags.js");
+setTimeout(run_tests, 1);
 
-function run_tests() {
-
-	for (var index in globalThis) {
-			var fn = globalThis[index];
-			t = ({}).toString.call(fn).toString();
-			if (t != "[object Function]") continue;
-			if (fn.name.substring(0, 5) != "test_") continue;
-
-			fn.call();
-
-	}
-}
-
-function assert() {
-	console.log("ass");
-}
-
-
-test_nope = () =>
+function run_tests()
 {
-    assert();
-    var name = ext.parse_game_id("dfdf");
-    console.log(name);
+    for (var index in globalThis) {
+        var fn = globalThis[index];
+        t = ({}).toString.call(fn).toString();
+        if (t != "[object Function]") continue;
+        if (fn.name.substring(0, 5) != "test_") continue;
+
+        case_name = fn.name;
+        fn.call();
+    }
 }
 
-run_tests();
+function assert(cond)
+{
+    var prefix = (cond ? "       [X]" : "FAIL - [ ]");
+    console.log("", prefix, case_name);
+}
+
+///////////////////////////////////////////////////////////////////////////////
+
+test_name_dekudeals = () =>
+{
+    var url = "https://www.dekudeals.com/items/the-last-campfire";
+    var site = ext.detect_site(url);
+    
+    assert(site == "dd");
+    var name = ext.parse_game_name(site, url);
+    
+    assert(name == "the_last_campfire");
+}
+
+test_name_nintendo_uk = () =>
+{
+    var url = "https://www.nintendo.co.uk/Games/Nintendo-Switch-games/The-Legend-of-Zelda-Tears-of-the-Kingdom-1576884.html";
+
+    var site = ext.detect_site(url);
+    assert(site == "uk");
+
+    var name = ext.parse_game_name(site, url);
+    assert(name == "the_legend_of_zelda_tears_of_the_kingdom");
+}
+
+test_name_nintendo_us = () =>
+{
+    var url = "https://www.nintendo.com/store/products/splatoon-3-switch/";
+
+    var site = ext.detect_site(url);
+    assert(site == "us");
+
+    var name = ext.parse_game_name(site, url);
+    assert(name == "splatoon_3_switch");
+}
