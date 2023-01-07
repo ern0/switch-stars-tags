ac5b07e9a06898cb11d870db4fa6965aacfb9a39
ern0 <ern0@linkbroker.hu>
2023-01-07 20:08:12 +0100

    Added page detection, with US reload watch

--- extension/script/switch-star-tags.js
+++ extension/script/switch-star-tags.js
@@ -1,9 +1,33 @@
-function init_extension() {
-	document.body.style.border = "12px solid blue";
+function init_extension()
+{
+	site = detect_site(window.location.href);
+	if (site == "us") {
+		last_url = "";
+		watch_page_us();
+	} else {
+		init_page();
+	}
 }
 
-function detect_site(url) {
+function watch_page_us()
+{
+	if (last_url != window.location.href) {
+		last_url = window.location.href;
+		init_page();
+	}
 
+	setTimeout(watch_page_us, 400);
+}
+
+function init_page()
+{
+	page = detect_page(site);
+
+	console.log("site: ", site, "page:", page);
+}
+
+function detect_site(url)
+{
 	if (url.includes("dekudeals.com/")) return "dd";
 	if (url.includes("nintendo.co.uk/")) return "uk";
 	if (url.includes("nintendo.com/")) return "us";
@@ -11,8 +35,8 @@ function detect_site(url) {
 	return "n.a.";
 }
 
-function parse_game_name(site, url) {
-
+function parse_game_name(site, url)
+{
 	if (url.slice(-1) == "/") url = url.slice(0, -1);
 	var lastSlashPos = url.lastIndexOf("/") + 1;
 	var name = "n.a.";
@@ -24,7 +48,36 @@ function parse_game_name(site, url) {
 	return name.toLowerCase().replaceAll("-", "_");
 }
 
-if (typeof(document) == "object") {
+function detect_page(site)
+{
+	if (site == "dd") return detect_page_dd();
+	if (site == "uk") return detect_page_uk();
+	if (site == "us") return detect_page_us();
+
+	return "n.a.";
+}
+
+function detect_page_dd() 
+{
+	var second = window.location.href.split("/")[3];
+	return (second == "items" ? "item" : "list");
+}
+
+
+function detect_page_uk()
+{
+	var second = window.location.href.split("/")[3].toLocaleLowerCase();
+	return (second == "games" ? "item" : "list");
+}
+
+function detect_page_us()
+{
+	var second = window.location.href.split("/")[4].toLocaleLowerCase();
+	return (second == "products" ? "item" : "list");
+}
+
+
+if (typeof (document) == "object") {
 	init_extension();
 } else {
 	module.exports = { parse_game_name, detect_site };
